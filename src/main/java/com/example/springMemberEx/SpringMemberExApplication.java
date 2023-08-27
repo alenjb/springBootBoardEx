0bb9156222737f6f23eb5b6425ab020e4f886955
alenjb <alenjb@naver.com>
2023-08-27 18:59:50 +0900

    [스프링 멤버 연습 프로젝트 생성]
    -개발 환경:
     springboot 3.1.3
     JDK 17
     MySQL 8.1.0
     dependencies: Lombok, spring Web, Thymleaf, MySQL Driver, MyBatis Framework
    
    - MySQL 연동 완료

---
+++ src/main/java/com/example/springMemberEx/SpringMemberExApplication.java
@@ -0,0 +1,13 @@
+package com.example.springMemberEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMemberExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMemberExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springMemberEx/SpringMemberExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMemberEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemberExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
